c0139a3d1490800f8fa87403ddc383e7585d445d
Zane Ravenholt <zravenholt@gmail.com>
2017-11-15 11:29:27 -0800

    hello world running

---
+++ server/index.js
@@ -0,0 +1,14 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let path = require('path');
+
+let app = express();
+let PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '../public')));
+//app.use('/bundles', express.static(path.join(__dirname, '../bundles')));
+
+app.listen(PORT, function() {
+  console.log('Sample form running on port: ', PORT);
+});
